916a6599931e9867119e2abb0518807b9e12d58d
Rohit Kumar <rohit96682@gmail.com>
2023-01-07 11:10:40 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.scss";
+import { FaHome } from "react-icons/fa";
+
+function App() {
+  return (
+    <h1 className="bg-success">
+      <FaHome /> Hello
+    </h1>
+  );
+}
+
+export default App;
