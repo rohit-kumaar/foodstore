70652b9032a54853628a8eb712a9735bc807af26
Rohit Kumar <rohit96682@gmail.com>
2023-01-08 12:08:58 +0530

    hero

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,21 @@
 import { useTitle } from "hooks/useTitle";
 import React from "react";
 import Header from "./Header";
+import { motion } from "framer-motion";
 
 function Home() {
   useTitle("Home");
+
+  const animation = {
+    initial: {
+      x: "-100%",
+      opacity: 0,
+    },
+    whileInView: {
+      x: 0,
+      opacity: 1,
+    },
+  };
 
   return (
     <>
@@ -11,11 +23,25 @@ function Home() {
 
       <section className="home">
         <div>
-          <h1>Foodtato</h1>
-          <p>Give yourself a tasty burger</p>
+          <motion.h1 {...animation}>Foodtato</motion.h1>
+          <motion.p
+            {...animation}
+            transition={{
+              delay: 0.2,
+            }}
+          >
+            Give yourself a tasty burger
+          </motion.p>
         </div>
 
-        <a href="#menu">Explore Menu</a>
+        <motion.a
+          href="#menu"
+          initial={{ y: "-100%", opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ delay: 0.4 }}
+        >
+          Explore Menu
+        </motion.a>
       </section>
     </>
   );
