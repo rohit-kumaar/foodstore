0583b525c75ed15c2eb709283ae9ca580c2e1894
Rohit Kumar <rohit96682@gmail.com>
2023-01-15 20:36:30 +0530

    container in hero

--- src/components/landing-page/Hero.jsx
+++ src/components/landing-page/Hero.jsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 function Hero() {
   return (
     <section className="hero">
-      <div>
+      <div className="container">
         <motion.h1 {...animation}>FoodSTORE</motion.h1>
         <motion.p
           {...animation}
@@ -15,17 +15,17 @@ function Hero() {
         >
           Give yourself a tasty burger
         </motion.p>
-      </div>
 
-      <motion.a
-        href="#menu"
-        initial={{ y: "-100%", opacity: 0 }}
-        whileInView={{ y: 0, opacity: 1 }}
-        transition={{ delay: 0.4 }}
-        className="btn"
-      >
-        Explore Menu
-      </motion.a>
+        <motion.a
+          href="#menu"
+          initial={{ y: "-100%", opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ delay: 0.4 }}
+          className="btn"
+        >
+          Explore Menu
+        </motion.a>
+      </div>
     </section>
   );
 }
