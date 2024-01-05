e3b26331e62cc788dd5b9adeb15a09ffd6df5b6d
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2024-01-05 17:51:36 +0530

    up to userReducer file

--- src/layouts/NotFound.jsx
+++ src/layouts/NotFound.jsx
@@ -2,6 +2,7 @@ import { useTitle } from "hooks/useTitle";
 import React from "react";
 import { MdError } from "react-icons/md";
 import { Link } from "react-router-dom";
+import { ROUTE_PATH } from "routes/routePath";
 
 function NotFound() {
   useTitle("Page Not Found");
@@ -15,7 +16,7 @@ function NotFound() {
 
         <p>Page Not Found, Click below to go to home page</p>
 
-        <Link to="/" className="btn">Go to home</Link>
+        <Link to={`${ROUTE_PATH.default}`} className="btn">Go to home</Link>
       </main>
     </>
   );
